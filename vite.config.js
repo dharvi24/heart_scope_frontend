a957f8f62dd1472d5925b48f4370ca902ee139ed
dharvi24 <dharvi.adthakkar10@gmail.com>
2026-02-27 19:33:15 +0530

    Fixed vite manualChunks error

--- vite.config.js
+++ vite.config.js
@@ -4,24 +4,24 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true,
-    host: true
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          animations: ['framer-motion']
-        }
-      }
-    }
-  },
-  css: {
-    devSourcemap: true
-  }
+  // server: {
+  //   port: 3000,
+  //   open: true,
+  //   host: true
+  // },
+  // build: {
+  //   outDir: 'dist',
+  //   sourcemap: true,
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks: {
+  //         vendor: ['react', 'react-dom', 'react-router-dom'],
+  //         animations: ['framer-motion']
+  //       }
+  //     }
+  //   }
+  // },
+  // css: {
+  //   devSourcemap: true
+  // }
 })
